88f7461ab4f52dda0c1c56290de6f713c24f690d
JhonBecerra69 <163014903+JhonBecerra69@users.noreply.github.com>
2024-09-18 08:31:11 -0500

    Update parcial1.py

--- PARCIAL1/parcial1.py
+++ PARCIAL1/parcial1.py
@@ -1,1 +1,5 @@
+"""
+Escribir un programa en python que permita clasificar triangulos (isosceles, escaleno, equilatero, rectangulo) apartir de 3 valores flotantes o double ingresados desde el teclado.
+definir y utilizar al menos una referencia de herencia que incluya encapsulamiento.
+"""
 
